4279c9b0cf6a2d83301e46e440b460b4d716ded2
DawidChec <checdd@gmail.com>
2022-03-18 15:30:52 +0100

    Green

--- src/test/java/TestEmployeeSystem.java
+++ src/test/java/TestEmployeeSystem.java
@@ -1,4 +1,9 @@
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
 import java.util.ArrayList;
 
 public class TestEmployeeSystem {
@@ -10,6 +15,26 @@ public class TestEmployeeSystem {
 
 
     @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Nu körs testsviten för Labb1 i TDD!");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("Nu avslutas testsviten!");
+    }
+
+    @AfterEach
+    public void afterEach() {
+        System.out.println("Testet har avslutats!");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("Testet har startats!");
+
+        testObject1 = new Employee("Dawid", "Chec", 25, 50.00);
+        testObjectSystem = new EmployeeSystem();
 
 
 
