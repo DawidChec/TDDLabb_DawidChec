a598a1ce94a751b70c6ed67cc64a7e28bef45e81
DawidChec <checdd@gmail.com>
2022-03-18 14:46:29 +0100

    Red

---
+++ src/main/java/Employee.java
@@ -0,0 +1,17 @@
+public class Employee {
+    private int ID;
+    private static int counter;
+    private String name;
+    private String lastName;
+    private int age;
+    private double salary;
+
+
+    public Employee(String name, String lastName, int age, double salary) {
+        counter = counter + 1;
+        ID = counter;
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+}
--- src/test/java/TestEmployee.java
+++ src/test/java/TestEmployee.java
@@ -1,2 +1,34 @@
+import org.junit.jupiter.api.*;
+
 public class TestEmployee {
+    Employee testObject1;
+    Employee testObject2;
+    Employee testObject3;
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Nu kör vi: ");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("Nu avslutar vi: ");
+    }
+
+    @AfterEach
+    public void afterEach() {
+        System.out.println("Slut: ");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("Start: ");
+
+        testObject1 = new Employee("Dawid", "Chec", 25, 50.00);
+        testObject2 = new Employee("Donald", "Trump", 56, 31050.00);
+        testObject1 = new Employee("Vladmir", "Putin", 66, 1.00);
+
 }
+    @Test
+    public void checkGetUniqueIDTest2() {
+        int actual2 = testObject2.getUniqueID();
