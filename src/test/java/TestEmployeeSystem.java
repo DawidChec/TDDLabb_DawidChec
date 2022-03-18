7ef9d0664209418e949265f07f94dc2cff50a924
DawidChec <checdd@gmail.com>
2022-03-18 15:26:38 +0100

    Red

--- src/test/java/TestEmployeeSystem.java
+++ src/test/java/TestEmployeeSystem.java
@@ -2,6 +2,14 @@
 import java.util.ArrayList;
 
 public class TestEmployeeSystem {
+    Employee testObject1;
+    Employee testObject2;
+    Employee testObject3;
+
+    EmployeeSystem testObjectSystem;
+
+
+    @BeforeAll
 
 
 
